cbf5ae24b3d56f834630302ee8ac3ce31d5ddef5
AXEL REYES <axel@advantedigital.com>
2024-02-05 01:25:58 -0600

    genetica

---
+++ interfaz.py
@@ -0,0 +1,107 @@
+import tkinter as tk
+from tkinter import filedialog, messagebox, ttk
+import threading
+import numpy as np
+import perceptron as pl
+
+def select_file():
+    filename = filedialog.askopenfilename(initialdir="/", title="SELECCIONAR ARCHIVO (CSV)",
+                                          filetypes=(("csv files", "*.csv"), ("all files", "*.*")))
+    file_label.config(text=filename)
+    return filename
+
+def start_training():
+    learning_rate = float(learning_rate_entry.get())
+    epochs = int(epochs_entry.get())
+    file_path = file_label.cget("text")
+    if file_path:  # Verificar si se ha seleccionado un archivo
+        threading.Thread(target=lambda: pl.train_perceptron(learning_rate, epochs, file_path, progress_bar)).start()
+    else:
+        messagebox.showwarning("Advertencia", "Debe seleccionar un archivo CSV.")
+
+def display_graphs():
+    pl.display_results()
+
+def generate_report():
+    np.set_printoptions(precision=4, suppress=True)
+    initial_weights, final_weights, epochs, error = pl.get_weights()
+    report_content = (f"EPOCAS: {epochs}\n"
+                      f"TASA DE APRENDIZAJE: {learning_rate_entry.get()}\n"
+                      f"ERROR PERMISIBLE: {error}\n\n"
+                      f"PESOS INICIALES DE W:\n{initial_weights}\n\n"
+                      f"PESOS FINALES DE W:\n{final_weights}")
+    # Asegurarse de que el widget de texto esté limpio antes de insertar el nuevo reporte
+    report_text.config(state="normal")
+    report_text.delete('1.0', tk.END)
+    report_text.insert(tk.END, report_content)
+    report_text.config(state="disabled")
+
+
+
+root = tk.Tk()
+root.title("Perceptron Training")
+root.geometry('600x400')  # Tamaño inicial de la ventana
+root.minsize(400, 750)  # Tamaño mínimo de la ventana
+
+
+# Estilo y tema
+style = ttk.Style()
+style.theme_use('alt')  # Cambiar a un tema diferente si se desea
+
+# Configuración de colores y fuentes
+bg_color = "#f0f0f0"
+text_color = "#333333"
+font = ('Helvetica', 10)
+
+# Configuración de la barra de progreso
+style.configure("TProgressbar", thickness=20, troughcolor=bg_color, bordercolor=bg_color, background='#333333')
+
+
+
+# Configurar el color de fondo de la ventana principal
+root.configure(bg=bg_color)
+
+# Frame principal con título
+labelframe = ttk.LabelFrame(root, text="INGRESAR DATOS", padding="20 20 20 20")
+labelframe.pack(fill="both", expand="yes", padx=20, pady=20)
+
+progress_buttons_frame = ttk.LabelFrame(root, text="Controles", padding="10 10 10 10")
+progress_buttons_frame.pack(fill="x", padx=20, pady=10)
+
+report_frame = ttk.LabelFrame(root, text="REPORTE", padding="10 10 10 10")
+report_frame.pack(fill="both", expand=True, padx=20, pady=10)  # Se expande con la ventana
+
+# Widget Text para mostrar el reporte
+report_text = tk.Text(report_frame, wrap="word", font=font, bg=bg_color, fg=text_color)
+report_text.pack(expand=True, fill="both", padx=10, pady=10)  # Llena el espacio disponible
+report_text.config(state="disabled")  # Inicia deshabilitado para que no sea editable
+
+
+# Widgets dentro del labelframe
+ttk.Label(labelframe, text="TASA DE APRENDIZAJE:", foreground=text_color, font=font).pack(pady=5)
+learning_rate_entry = ttk.Entry(labelframe, width=50)
+learning_rate_entry.pack()
+
+ttk.Label(labelframe, text="EPOCAS (ITERACIONES):", foreground=text_color, font=font).pack(pady=5)
+epochs_entry = ttk.Entry(labelframe, width=50)
+epochs_entry.pack()
+
+# Frame para mostrar el nombre del archivo seleccionado
+file_frame = ttk.Frame(labelframe, padding="10 10 10 10")
+file_frame.pack(fill="x", expand="yes")
+ttk.Button(file_frame, text="Select CSV File", command=select_file).pack(side="left", padx=5)
+file_label = ttk.Label(file_frame, text="", background=bg_color, foreground=text_color, font=font)
+file_label.pack(side="left", fill="x", expand=True)
+
+# Botones centrados
+buttons_frame = ttk.Frame(labelframe)
+buttons_frame.pack(pady=20)
+ttk.Button(buttons_frame, text="INICIAR", command=start_training).pack(side="left", padx=5)
+ttk.Button(buttons_frame, text="GRÁFICAS", command=display_graphs).pack(side="left", padx=5)
+ttk.Button(labelframe, text="REPORTE", command=generate_report).pack(pady=10)
+
+# Barra de progreso
+progress_bar = ttk.Progressbar(labelframe, orient="horizontal", mode="determinate", style="TProgressbar")
+progress_bar.pack(fill="x", pady=10)
+
+root.mainloop()
---
+++ perceptron.py
@@ -0,0 +1,73 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Variables to store training results
+error_norm_epochs = []
+weight_evolution = []
+initial_weights = None
+final_weights = None
+num_epochs = 0
+permissible_error = 0
+
+
+def train_perceptron(learning_rate, epochs, file_path, progress_bar):
+    global error_norm_epochs, weight_evolution, initial_weights, num_epochs, final_weights
+
+    error_norm_epochs, weight_evolution = [], []
+    data_frame = pd.read_csv(file_path, delimiter=';', header=None)
+    num_features = data_frame.shape[1] - 1
+
+    weights = np.random.uniform(0, 1, (num_features + 1, 1)).round(4)
+    x_columns = np.hstack([data_frame.iloc[:, :-1].values, np.ones((data_frame.shape[0], 1))])
+    y_column = data_frame.iloc[:, -1].values.reshape(-1, 1)
+
+    initial_weights = weights.copy()
+    num_epochs = epochs
+    print(f"Initial weights: {weights.T}")
+
+    for _ in range(num_features + 1):
+        weight_evolution.append([])
+
+    for epoch in range(epochs):
+        calculated_y = np.where(x_columns @ weights >= 0, 1, 0).reshape(-1, 1)
+        errors = y_column - calculated_y
+        error_norm_epochs.append(np.linalg.norm(errors))
+
+        for i in range(num_features + 1):
+            weight_evolution[i].append(weights[i, 0])
+
+        weights += learning_rate * (x_columns.T @ errors)
+        weights = np.round(weights, 4)
+
+        # Update the progress bar
+        progress_bar['value'] = (epoch + 1) / epochs * 100
+        progress_bar.update()
+
+    final_weights = weights
+
+
+def display_results():
+
+    plt.style.use('seaborn-v0_8') 
+    global error_norm_epochs, weight_evolution
+    plt.figure(figsize=(10, 4))
+    plt.subplot(1, 2, 1)
+    plt.plot(range(1, len(error_norm_epochs) + 1), error_norm_epochs)
+    plt.title('Evolución del Error Absoluto(|e|)')
+    plt.xlabel('Épocas')
+    plt.ylabel('Error Absoluto(|e|)')
+
+    plt.subplot(1, 2, 2)
+    for i, weight_epoch in enumerate(weight_evolution):
+        plt.plot(range(1, len(weight_epoch) + 1), weight_epoch, label=f'W {i + 1}')
+    plt.title('Evolución de Pesos (W)')
+    plt.xlabel('Epocas')
+    plt.ylabel('Valor de W')
+    plt.legend()
+
+    plt.tight_layout()
+    plt.show()
+
+def get_weights():
+    return initial_weights, final_weights, num_epochs, permissible_error
